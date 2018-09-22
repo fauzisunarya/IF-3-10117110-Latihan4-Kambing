470685f4302e67ff32c876d413b297aba998dc69
fauzisunarya <fauzialdzikirullah@gmail.com>
2018-09-22 17:00:13 +0700

    first commit

---
+++ src/pkgif/pkg3/pkg10117110/latihan4/kambing/IF310117110Latihan4Kambing.java
@@ -0,0 +1,31 @@
+
+package pkgif.pkg3.pkg10117110.latihan4.kambing;
+/**
+ *
+ * @author
+ * Nama : Fauzi Sunarya
+ * Kelas : IF-3
+ * NIM : 10117110
+ * Deskripsi Program : Program Jenis ini Berisi Program Untuk Menampilkan
+ * jumlah Kambing
+ */
+public class IF310117110Latihan4Kambing {
+    
+         public void tambahkambing(){    
+        // Deklarasi Variabel Lokal
+        int jumlahKambing = 0;
+        
+        jumlahKambing = jumlahKambing + 5;
+        System.out.println("Jumlah Kambing Setelah ditambah: " + jumlahKambing);      
+
+    }
+
+
+    public static void main(String[] args) {
+           
+          IF310117110Latihan4Kambing kambingJantan = new IF310117110Latihan4Kambing();
+          kambingJantan.tambahkambing();
+ 
+    }
+    
+}
